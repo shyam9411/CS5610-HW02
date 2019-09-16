5b51daec607f66245e5c69b7571e256e0f845f78
Shyam Kumar <shyam9411@guest.guest>
2019-09-16 20:00:17 +0000

    Added attribution and relevant comments in the code base

--- calc/calc.js
+++ calc/calc.js
@@ -5,6 +5,10 @@
     let operation;
     const calc = {};
 
+    /*
+     * Method to initialize the calculator by registering for click events for all the calculator function methods.
+     * @return None
+     */
     calc.initializeCalc = function() {
         leftValue = "";
         rightValue = "";
@@ -14,11 +18,24 @@
             calcFunctionEle[index].addEventListener("click", calc.clickHandler)
     }
 
+    /*
+     * Method to display corresponding text on the calculator text box.
+     * 
+     * @param calcValue display text in calculator
+     * @private
+     */
     calc._displayOnCalc = function(calcValue) {
         let displayEle = document.getElementById("displayText");
         displayEle.value = calcValue;
     }
 
+    /*
+     * Method to handle the function buttons of calculator. It also determines when to compute ALU operations over other calculator operations.
+     *
+     * @param op determines the operation to be performed in calculator.
+     * @return true when current operations have been handled, false otherwise.
+     * @private
+     */
     calc._handlerOperation = function(op) {
         let bHandled = true;
 
@@ -49,6 +66,13 @@
         return bHandled;
     }
 
+    /*
+     * Performs ALU operations on the current state of the calculator. It uses two operands namely leftValue, rightValue and uses the current operation on them. The result is saved as a part of leftValue upon processing.
+     *
+     * @param op determines the operation to be performed on the operands
+     * @return None
+     * @private
+     */
     calc._performALUOperation = function(op) {
         if (operation === null)
             return;
@@ -84,6 +108,12 @@
 	operation = op;
     }
 
+    /*
+     * Method to handle the click events on calculator funcitons.
+     *
+     * @param event determines the event object sent when onClick is triggered on each of the controls.
+     * @return None
+     */
     calc.clickHandler = function(event) {
         let calcValue = "";
         let clickedValue = event.target.innerText;
@@ -105,7 +135,9 @@
         calc._displayOnCalc(calcValue);
     }
 
+   // Adding the current calc object as a valid namespace
    global.calc = calc;
 })(window);
 
+// Initialize calculator upon window completely loaded
 window.addEventListener("load", window.calc.initializeCalc);
